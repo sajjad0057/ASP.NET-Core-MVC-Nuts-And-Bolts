36cd8a1e71ac292227f0b6d3b54e4207f1580d08
sajjad0057 <sajjadhossain0057@gmail.com>
2022-10-09 22:19:16 +0600

    Using ADO.NET for Connecting Database and perform Operations (Example - Set Db Connection String HardCoded )

---
+++ ASP.NET Core MVC/FirstDemo.Infrastructure/Services/DataUtility.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDemo.Infrastructure.Services
+{
+    public class DataUtility
+    {
+        public static void InsertData()
+        {
+
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = "Server = .\\SQLEXPRESS; Database = Asp.netCoreMvc; User Id = Asp.netCoreMvc; Password = 123456;";
+
+
+        }
+    }
+}
