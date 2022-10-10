3a7e9fcc42f4ae8c9ff92c21dbf57a3cd4677660
sajjad0057 <sajjadhossain0057@gmail.com>
2022-10-10 17:20:03 +0600

    Way - 1 Read data using ADO.NET

--- ASP.NET Core MVC/FirstDemo.Infrastructure/Services/DataUtility.cs
+++ ASP.NET Core MVC/FirstDemo.Infrastructure/Services/DataUtility.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace FirstDemo.Infrastructure.Services
 {
@@ -21,30 +22,40 @@ namespace FirstDemo.Infrastructure.Services
             _connectionString = config.GetConnectionString("DefaultConnection");
             
         }
+
+
+        private SqlCommand _PrepareCommand(string sql, Dictionary<string, object> parameters)
+        {
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = _connectionString;
+
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.Connection = sqlConnection;
+            sqlCommand.CommandText = sql;
+
+
+            if (parameters != null)
+            {
+                foreach (var item in parameters)
+                {
+                    sqlCommand.Parameters.Add(new SqlParameter(item.Key, item.Value));
+                }
+            }
+
+
+            return sqlCommand;
+
+        }
         public async Task ExecuteCommandAsync(string command,Dictionary<string,object> parameters)
         {
 
-            using SqlConnection sqlConnection = new SqlConnection();
-            sqlConnection.ConnectionString = _connectionString;
-
-            using SqlCommand sqlCommand = sqlConnection.CreateCommand();
-            sqlCommand.Connection = sqlConnection;
-            sqlCommand.CommandText = command;
-                
+            using SqlCommand sqlCommand = _PrepareCommand(command, parameters);  
 
             try
             {
-                if(sqlConnection.State != System.Data.ConnectionState.Open)
+                if(sqlCommand.Connection.State != System.Data.ConnectionState.Open)
                 {
-                    sqlConnection.Open();   
-                }
-
-                if(parameters != null)
-                {
-                    foreach(var item in parameters)
-                    {
-                        sqlCommand.Parameters.Add(new SqlParameter(item.Key,item.Value));
-                    }
+                    sqlCommand.Connection.Open();   
                 }
 
                 int impact = await sqlCommand.ExecuteNonQueryAsync();
@@ -58,6 +69,12 @@ namespace FirstDemo.Infrastructure.Services
 
             }
 
+
+        }
+
+
+        public void GetDataAsync(string command, Dictionary<string, object> parameters)
+        {
 
         }
 
