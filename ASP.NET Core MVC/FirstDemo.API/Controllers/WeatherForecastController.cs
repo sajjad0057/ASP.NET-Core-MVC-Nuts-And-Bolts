3e39277d489a8763022a2d41fd41ae142b7e7402
sajjad0057 <sajjadhossain0057@gmail.com>
2022-11-16 01:22:43 +0600

    Config (autofac,serilog and initial authentication) for FirstDemo.API project and get auth token successfully updated

--- ASP.NET Core MVC/FirstDemo.API/Controllers/WeatherForecastController.cs
+++ ASP.NET Core MVC/FirstDemo.API/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FirstDemo.API.Controllers
@@ -19,6 +20,7 @@ namespace FirstDemo.API.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
+        [Authorize(Policy = "CourseViewRequirementPolicy")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
