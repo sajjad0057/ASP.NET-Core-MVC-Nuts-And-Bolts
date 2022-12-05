7d4270cffd2bde464457c6dead4e504ad372a34b
sajjad0057 <sajjadhossain0057@gmail.com>
2022-12-06 04:13:45 +0600

    Manage basic configuration (Serilog,Autofac) in FirstDemo.Worker project and successfully write log in file

--- ASP.NET Core MVC/FirstDemo.Worker/Program.cs
+++ ASP.NET Core MVC/FirstDemo.Worker/Program.cs
@@ -1,10 +1,59 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using FirstDemo.Worker;
+using Serilog;
+using Serilog.Events;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
-    {
-        services.AddHostedService<Worker>();
-    })
-    .Build();
 
-await host.RunAsync();
+
+
+#region Accessing_AppSettings.json_file
+var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", false)
+                .AddEnvironmentVariables()
+                .Build();
+
+var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+#endregion
+
+var migrationAssemblyName = typeof(Worker).Assembly.FullName;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+try
+{
+    Log.Information("Application Starting up");
+
+    #region ForResolving_DependencyInjection_using_Autofac
+
+    IHost host = Host.CreateDefaultBuilder(args)
+        .UseWindowsService()
+        .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+        .UseSerilog()
+        .ConfigureContainer<ContainerBuilder>(builder =>
+        {
+            builder.RegisterModule(new WorkerModule());
+        })
+        .ConfigureServices(services =>
+        {
+            services.AddHostedService<Worker>();
+        })
+        .Build();
+
+    #endregion
+
+    await host.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application start-up failed");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
---
+++ ASP.NET Core MVC/FirstDemo.Worker/WorkerModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+
+namespace FirstDemo.Worker
+{
+    public class WorkerModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            /*
+            builder.RegisterType<CourseModel>().As<ICourseModel>()
+                .InstancePerLifetimeScope();
+            builder.RegisterType<CourseModel>().AsSelf();
+            builder.RegisterType<CourseCreateModel>().AsSelf();
+            builder.RegisterType<CourseEditModel>().AsSelf();
+            builder.RegisterType<CourseListModel>().AsSelf();
+            builder.RegisterType<RegisterModel>().AsSelf();
+            builder.RegisterType<LoginModel>().AsSelf();
+            */
+            base.Load(builder);
+        }
+    }
+}
