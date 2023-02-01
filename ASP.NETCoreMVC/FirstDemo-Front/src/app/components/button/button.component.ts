e73351629a0f49282ea82fdf6e718bbaa18218ce
sajjad0057 <sajjadhossain0057@gmail.com>
2023-02-01 21:32:48 +0600

    Init Angular and make button

---
+++ ASP.NETCoreMVC/FirstDemo-Front/src/app/components/button/button.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+  @Input() text:string;
+  @Input() color:string;
+
+  constructor(){
+    this.text = "";
+    this.color = ""
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  onClick(){
+    console.log("button clicked !")
+  }
+}
